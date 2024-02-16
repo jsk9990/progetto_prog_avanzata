d7c200ba68c343c164fcea74290edd10a6721e5e
jsk <jsk.ubuntu99@gmail.com>
2024-02-16 15:21:27 +0100

    Creazione modello Utente

--- Model/utente.ts
+++ Model/utente.ts
@@ -1,1 +1,43 @@
+import {DataTypes,Sequelize} from 'sequelize'; //importo sequelize 
+import {singleton}  from './singleton'; //import singleton 
 
+
+const sequelize: Sequelize = singleton.getConnection();
+
+export const Utente = sequelize.define('Utente', {
+    id_utente: {
+        type:DataTypes.INTEGER(),
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    email: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+    password: {
+        type:DataTypes.STRING(45),
+        allowNull: false
+    },
+    credito :{
+        type:DataTypes.FLOAT(),
+        allowNull: false
+    },
+    privilegi: {
+        type:DataTypes.BOOLEAN(),
+        allowNull: false
+    },
+    create_time: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+  
+},
+
+{ 
+    timestamps: false,
+    freezeTableName: true
+}); 
+
+module.exports = { Utente: Utente };
