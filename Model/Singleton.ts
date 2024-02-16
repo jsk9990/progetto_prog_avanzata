34ee157016d6c6cb69c8595469c88e75dd4cf7b3
jsk <jsk.ubuntu99@gmail.com>
2024-02-16 17:22:28 +0100

    Commit

--- Model/Singleton.ts
+++ Model/Singleton.ts
@@ -5,11 +5,7 @@ export class Singleton {
     private static instance: Singleton;
     private connection: Sequelize;
     private constructor() {
-<<<<<<< HEAD
         this.connection = new Sequelize('mydb','jsk','jsk',{
-=======
-        this.connection = new Sequelize('Blog','jsk','jsk',{
->>>>>>> d05959c72f989ed5a3ab47038078705a145fda92
             host : 'localhost',
             dialect : 'mysql'
         }); 
